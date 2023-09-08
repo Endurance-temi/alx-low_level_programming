cc5471d07396b1f61df3cdab4885fa64405143b1
Endurance-temi <temit682@gmail.com>
2023-09-07 20:24:43 -0700

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - it prints out using put.
+ *
+ * Return: 0 if success.
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
