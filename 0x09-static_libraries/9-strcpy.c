5c7a0a4270fb6b9730d9374fa4bbfbb50457aa2a
Endurance-temi <temit682@gmail.com>
2023-10-03 07:36:24 -0700

    0-memset.c   100-atoi.o   1-strncat.c  2-strlen.o   3-puts.c     3-strspn.o   5-strstr.c   9-strcpy.o  _putchar.o

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *char  *_strcpy - a function that copies the string pointed to by src.
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
+	dest[a] = '\0';
+
+	return (dest);
+}
