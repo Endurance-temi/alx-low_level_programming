05b9d1fac080c01ca5d7d8efe408127e3f32fd69
Endurance-temi <temit682@gmail.com>
2023-10-24 14:06:55 -0700

    prints all elements of a linked list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -6,8 +6,8 @@
 
 /**
  * struct listint_s - singly linked list
- *@n: integer
- *
+ * @n: integer
+ * @next: self referencial pointer
  * Description: singly linked list node structure
  * for alx
  */
@@ -17,6 +17,18 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * struct listp_s - singly linked list
+ *@p: pointers of nodes
+ *@next: points to the next node
+ *
+ * Desc: singly linked list pointers
+ */
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
